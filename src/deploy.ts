b3323880aab84f54875b1699cb6e6b223fed5ab3
aagargoura <ahmed.gargoura@gmail.com>
2025-11-08 19:35:04 +0100

    Add import statements to access required libraries

---
+++ src/deploy.ts
@@ -0,0 +1,22 @@
+import { WalletBuilder } from "@midnight-ntwrk/wallet";
+import { deployContract } from "@midnight-ntwrk/midnight-js-contracts";
+import { httpClientProofProvider } from "@midnight-ntwrk/midnight-js-http-client-proof-provider";
+import { indexerPublicDataProvider } from "@midnight-ntwrk/midnight-js-indexer-public-data-provider";
+import { NodeZkConfigProvider } from "@midnight-ntwrk/midnight-js-node-zk-config-provider";
+import { levelPrivateStateProvider } from "@midnight-ntwrk/midnight-js-level-private-state-provider";
+import {
+  NetworkId,
+  setNetworkId,
+  getZswapNetworkId,
+  getLedgerNetworkId
+} from "@midnight-ntwrk/midnight-js-network-id";
+import { createBalancedTx } from "@midnight-ntwrk/midnight-js-types";
+import { nativeToken, Transaction } from "@midnight-ntwrk/ledger";
+import { Transaction as ZswapTransaction } from "@midnight-ntwrk/zswap";
+import { WebSocket } from "ws";
+import * as fs from "fs";
+import * as path from "path";
+import * as readline from "readline/promises";
+import * as Rx from "rxjs";
+import { type Wallet } from "@midnight-ntwrk/wallet-api";
+
